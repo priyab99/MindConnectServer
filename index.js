cea9384aa88d4e4c1dc73e195d1cc771ee1c6204
priyab99 <priya.bd.9911@gmail.com>
2023-09-16 22:44:34 +0600

    user collected

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=express();
 const cors = require('cors');
 require('dotenv').config()
 const port=process.env.PORT || 5000;
-
+const jwt=require('jsonwebtoken')
 
 //middlewares
 
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s3cfwic.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,10 +33,45 @@ async function run() {
 
 
   //user related api
+   app.get('/users',async(req,res)=>{
+    const result=await usersCollection.find().toArray();
+    res.send(result);
+   })
+
   app.post('/users', async (req, res) => {
     const user = req.body;
     const result = await usersCollection.insertOne(user);
     res.send(result);
+});
+
+ // Update user role as admin
+ app.patch('/users/admin/:id', async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const filter = { _id: new ObjectId(id) };
+  const updateDoc = {
+      $set: {
+          role: 'admin'
+      },
+  };
+
+  const result = await usersCollection.updateOne(filter, updateDoc);
+  res.send(result);
+})
+
+
+// Update user role as therapist
+app.patch('/users/therapist/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const updateDoc = {
+      $set: {
+          role: 'therapist'
+      },
+  };
+
+  const result = await usersCollection.updateOne(filter, updateDoc);
+  res.send(result);
 });
 
     //symptoms related api
